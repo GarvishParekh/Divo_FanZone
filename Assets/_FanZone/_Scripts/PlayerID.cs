c1e5bb69af1a8a0a9f7a2a8f47b906c74b982bd4
Garvish Parekh <garvish.p@theintellify.com>
2023-09-12 15:35:31 +0530

    Player ID Update

--- Assets/_FanZone/_Scripts/PlayerID.cs
+++ Assets/_FanZone/_Scripts/PlayerID.cs
@@ -1,14 +1,19 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class PlayerID : MonoBehaviour
 {
     public int playerID;
+    [SerializeField] private PhotonView photonView;
     [SerializeField] private GameObject C_playerID;
     
     public bool isPlayer = false;
 
     public void SetPlayerID (int _id)
     {
+        if (!photonView.IsMine) return;
+
+
         playerID = _id;
         C_playerID.SetActive(false);
     }
